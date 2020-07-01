4de7a58bd0d0fc4f6a9b082425b21f009f9f80e8
Petar Maymounkov <petarm@gmail.com>
2020-06-30 21:56:50 -0700

    address comments

--- cmd/ipfs/rotate.go
+++ cmd/ipfs/rotate.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"time"
 
 	cmds "github.com/ipfs/go-ipfs-cmds"
 	config "github.com/ipfs/go-ipfs-config"
 	oldcmds "github.com/ipfs/go-ipfs/commands"
 	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
 	"github.com/ipfs/interface-go-ipfs-core/options"
+	ci "github.com/libp2p/go-libp2p-core/crypto"
+	b58 "github.com/mr-tron/base58/base58"
+)
+
+const (
+	algorithmDefault    = options.Ed25519Key
+	algorithmOptionName = "algorithm"
 )
 
 var rotateCmd = &cmds.Command{
@@ -27,6 +36,7 @@ environment variable:
 	},
 	Arguments: []cmds.Argument{},
 	Options: []cmds.Option{
+		cmds.StringOption(algorithmOptionName, "a", "Cryptographic algorithm to use for key generation.").WithDefault(algorithmDefault),
 		cmds.IntOption(bitsOptionName, "b", "Number of bits to use in the generated RSA private key.").WithDefault(nBitsForKeypairDefault),
 	},
 	PreRun: func(req *cmds.Request, env cmds.Environment) error {
@@ -48,15 +58,13 @@ environment variable:
 	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
 		cctx := env.(*oldcmds.Context)
 		nBitsForKeypair, _ := req.Options[bitsOptionName].(int)
-		return doRotate(os.Stdout, cctx.ConfigRoot, nBitsForKeypair)
+		algorithm, _ := req.Options[algorithmOptionName].(string)
+		return doRotate(os.Stdout, cctx.ConfigRoot, algorithm, nBitsForKeypair)
 	},
 }
 
-func doRotate(out io.Writer, repoRoot string, nBitsForKeypair int) error {
+func doRotate(out io.Writer, repoRoot string, algorithm string, nBitsForKeypair int) error {
 	// Open repo
-	if err := checkWritable(repoRoot); err != nil {
-		return err
-	}
 	repo, err := fsrepo.Open(repoRoot)
 	if err != nil {
 		return err
@@ -69,23 +77,54 @@ func doRotate(out io.Writer, repoRoot string, nBitsForKeypair int) error {
 		return err
 	}
 
+	// Generate new identity
+	identity, err := config.CreateIdentity(out, []options.KeyGenerateOption{
+		options.Key.Size(nBitsForKeypair),
+		options.Key.Type(algorithm),
+	})
+	if err != nil {
+		return err
+	}
+	newPrivKey, err := identity.DecodePrivateKey("") //XXX
+	if err != nil {
+		return err
+	}
+
 	// Save old identity to keystore
-	privKey, err := cfg.Identity.DecodePrivateKey("") //XXX
+	oldPrivKey, err := cfg.Identity.DecodePrivateKey("") //XXX
 	if err != nil {
 		return err
 	}
 	keystore := repo.Keystore()
-	if err := keystore.Put(cfg.Identity.PeerID, privKey); err != nil {
-		return err
-	}
-
-	// Generate new identity
-	identity, err := config.CreateIdentity(out, []options.KeyGenerateOption{options.Key.Size(nBitsForKeypair)})
+	oldKey, err := pubKeyPrefix(oldPrivKey.GetPublic())
 	if err != nil {
 		return err
 	}
+	newKey, err := pubKeyPrefix(newPrivKey.GetPublic())
+	if err != nil {
+		return err
+	}
+	name := fmt.Sprintf("IdentityRotation-%s-from-%s-to-%s-at-%s",
+		cfg.Identity.PeerID,
+		oldKey,
+		newKey,
+		time.Now().Format(time.RFC822),
+	)
+	if err := keystore.Put(name, oldPrivKey); err != nil {
+		return err
+	}
+
+	// Update identity
 	cfg.Identity = identity
 
 	// Write config file to repo
 	return repo.SetConfig(cfg)
+}
+
+func pubKeyPrefix(k ci.PubKey) (string, error) {
+	r, err := k.Raw()
+	if err != nil {
+		return "", err
+	}
+	return b58.Encode(r), nil
 }
